7c6bbbc5dd4da0e802ef7b985ac024cc8f747744
Lamhotjm <lamhotjms@gmail.com>
2020-04-07 14:22:48 -0500

    init lab10

---
+++ Lab_10_EAI/src/main/java/edu/mum/amqp/ItemListener.java
@@ -0,0 +1,14 @@
+package edu.mum.amqp;
+
+import edu.mum.domain.Item;
+
+public class ItemListener {
+
+	public void listen(Item item) {
+		String name = item.getName();
+		String desc = item.getDescription();
+		System.out.println("---------- Direct consumer for Item: " + name + " desc" + desc);
+	}
+
+
+}
